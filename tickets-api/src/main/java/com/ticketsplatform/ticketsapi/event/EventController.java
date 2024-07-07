22fd8e0f5e67d39f98a4581483490e99569f3eb3
Gabriel Spiandorello <gabrielspiandorello@gmail.com>
2024-07-07 02:15:28 -0300

    Test deploy ec2

--- tickets-api/src/main/java/com/ticketsplatform/ticketsapi/event/EventController.java
+++ tickets-api/src/main/java/com/ticketsplatform/ticketsapi/event/EventController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ticketsplatform.ticketsapi.event.dtos.EventDTO;
 import com.ticketsplatform.ticketsapi.event.dtos.EventRequestDTO;
 
